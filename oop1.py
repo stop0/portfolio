71d510503a616edbbcc33653033b400c004f5e4e
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2019-04-29 00:44:46 -0400

    OOP part 1

---
+++ oop1.py
@@ -0,0 +1,26 @@
+# Object-Orinted Programming
+
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@gmail.com'
+
+    def fullName(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Corey', 'Scahafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+# print(emp_1)
+# print(emp_2)
+
+
+print(emp_1.email)
+print(emp_2.email)
+
+print(emp_1.fullName())
+print(emp_2.fullName())
